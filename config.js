7065678c344016de8ecebb16fdb5f2e3559a17ad
duwanli <duwl@lybrc.com.cn>
2018-03-16 17:09:12 +0800

    爬虫

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // 基本参数
 const config = {
 	emailService: 'QQ',
-	senderEmail: '872183144@qq.com',// 发件人邮箱账号
-	senderPass: 'xwtnaswozyakbfad', // 发件人邮箱密码
-	addresseeEmail: '872183144@qq.com', // 收件人邮箱账号
+	senderEmail: '',// 发件人邮箱账号
+	senderPass: '', // 发件人邮箱密码
+	addresseeEmail: '', // 收件人邮箱账号
 	sendUrl: 'https://www.huobi.com/p/api/contents/pro/list_notice?r=ald0uo2k0wf&limit=10&language=zh-cn',// 爬取数据的接口URL
 
 }
